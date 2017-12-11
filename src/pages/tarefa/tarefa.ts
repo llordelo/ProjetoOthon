898ab052a3ed458c515965d4a13533668cdcda3f
llordelo <llordelo94@gmail.com>
2017-12-11 15:18:18 -0300

    Telas de tarefa e detalhe tarefa criadas

--- src/pages/tarefa/tarefa.ts
+++ src/pages/tarefa/tarefa.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { DetalheTarefaPage } from '../detalhe-tarefa/detalhe-tarefa';
 
 /**
  * Generated class for the TarefaPage page.
@@ -25,5 +26,21 @@ export class TarefaPage {
     this.parameter1 = this.navParams.get('param1');
     console.log(this.parameter1);
   }
+
+  nextPage() {
+    this.navCtrl.push(DetalheTarefaPage);
+  }
+
+   items = [
+     'Tarefa 1',
+     'Tarefa 2',
+     'Tarefa 3'
+   ];
+
+   itemSelected(item: string) {
+     console.log("Selected Item", item);
+     this.navCtrl.push(DetalheTarefaPage, {param1:item}); 
+  }
+
 
 }
